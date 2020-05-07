8d4ab5a7854c4395d7e499b9cb3fc9ceb25b9130
Wesley Reed <reedws@ucmail.uc.edu>
2020-05-07 14:38:40 -0400

    add bootstrap vue

---
+++ src/plugins/bootstrap-vue.js
@@ -0,0 +1,7 @@
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
