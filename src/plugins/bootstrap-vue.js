7a9e4a7ab931f65504cc67c3ed3993a04ce49042
Wesley Reed <reedws@ucmail.uc.edu>
2020-06-02 16:12:09 -0400

    initial datatable

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -2,8 +2,7 @@ import 'mutationobserver-shim';
 import Vue from 'vue';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@/styles/theme.scss';
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
