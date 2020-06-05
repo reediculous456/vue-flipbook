31238136dee2bc5a5c52031223d58d9310f0bacd
Wesley Reed <reedws@ucmail.uc.edu>
2020-06-05 16:00:31 -0400

    clicking help hover no longer checks box

--- src/components/PageEdit.vue
+++ src/components/PageEdit.vue
@@ -2,23 +2,26 @@
   <b-card>
     <p>Page: {{ page.page_number }}</p>
     <b-form-group>
-      <b-form-checkbox v-model="page.is_double">
+      <b-form-checkbox
+        v-model="page.is_double"
+        inline
+      >
         is Double Page
-        <b-icon
-          :id="`doublePage${page.page_number}`"
-          icon="question-circle-fill"
-          variant="primary"
-        />
-        <b-tooltip
-          :target="`doublePage${page.page_number}`"
-          variant="primary"
-          placement="bottom"
-          triggers="hover"
-        >
-          Some PDFs are exported in booklet form.
-          If this page in the PDF you uploaded is really 2 pages in print, check this box
-        </b-tooltip>
       </b-form-checkbox>
+      <b-icon
+        :id="`doublePage${page.page_number}`"
+        icon="question-circle-fill"
+        variant="primary"
+      />
+      <b-tooltip
+        :target="`doublePage${page.page_number}`"
+        variant="primary"
+        placement="bottom"
+        triggers="hover"
+      >
+        Some PDFs are exported in booklet form.
+        If this page in the PDF you uploaded is really 2 pages in print, check this box
+      </b-tooltip>
     </b-form-group>
     <b-form-group label="Alt text">
       <b-form-textarea
